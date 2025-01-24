47a9dc2f07b4bcfe7a539f8928b3b7910a182d51
nrennie <nrennie35@gmail.com>
2025-01-24 10:59:39 +0000

    skeleton app

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+			      fallback: '404.html'
+		    }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/d3' : '',
+        }
+    }
+};
+
+export default config;
